80a19e6adff6293bbbfdc0f161847d59990aa90f
morshedul-antor <antor.morshedul@gmail.com>
2022-06-16 12:25:45 +0600

    fix: medicine import into echamber database

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import mysql.connector
 
 i = 1
 
-def insert(brand_name, generic_name, form, strength, pharmaceutical):
+def insert(name, generic, form, strength, pharmaceuticals, unit_type, unit_price):
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="medicinedb"
+    database="echamber"
     )
 
     mycursor = mydb.cursor()
 
-    sql = "INSERT INTO medicines (brand_name, generic_name, form, strength, pharmaceutical) VALUES (%s, %s, %s, %s, %s )"
-    val = (brand_name, generic_name, form, strength, pharmaceutical)
+    sql = "INSERT INTO ep_medicine_list (name, generic, form, strength, pharmaceuticals, unit_type, unit_price) VALUES (%s, %s, %s, %s, %s, %s, %s )"
+    val = (name, generic, form, strength, pharmaceuticals, unit_type, unit_price)
 
     mycursor.execute(sql, val)
 
     mydb.commit()
 
     global i
-    print(f"[{i}] {brand_name} = record inserted.")
+    print(f"[{i}] {name} = record inserted.")
     i += 1
 
 
@@ -28,7 +28,14 @@ def app():
     with open('medx.txt') as f:
         for line in f:
             l = line.split('|')
-            insert(l[0].strip(),l[1].strip(),l[2].strip(),l[3].strip(),l[4].strip())
+            s = l[5].split(': ৳')
+
+            if len(s) < 2: 
+                ss = 0.0
+            else:
+                ss = float(s[1].strip().replace("\n",'').replace(",",''))
+
+            insert(l[0].strip(), l[1].strip(), l[2].strip(), l[3].strip(), l[4].strip(), s[0].strip(), ss)
 
 if __name__ == '__main__':
     app()
